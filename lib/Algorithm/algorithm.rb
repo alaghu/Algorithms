6dc3d3cdef104092b87c6378fda7597ebcf34f4a
Anand <anand@loremore.com>
2013-12-29 21:34:48 -0800

    understanding the do block.

--- lib/Algorithm/algorithm.rb
+++ lib/Algorithm/algorithm.rb
@@ -1,14 +1,33 @@
 # Defining a class Algorithm.
 class Algorithm
+
+  #defining a method to find the largest integer
   def find_largest_integer(*integers)
-    largest = 0
+
+    puts "The length of integers is #{integers.length}"
 
     integers.each do |integer_iteration|
 
-      largest = integer_iteration if integer_iteration > largest
+      puts "The integer = #{integers}"
+      puts "The integer_iteration = #{integer_iteration}"
+
+      #largest = integer_iteration
+      puts integers.length
+      puts largest
+
 
     end
 
     largest
   end
 end
+
+# Calling this as a simple test
+
+array = [1, 4, 5, 6]
+
+puts "This is the length of the array #{array.length}"
+
+largest_number = Algorithm.new
+
+largest_number.find_largest_integer(array)
