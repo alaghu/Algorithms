6731c7d4f2aa17f188b5b872c0eaba2a423858e9
Anand <anand@loremore.com>
2014-01-13 22:02:15 -0800

    Added check for integer. Need to refactor method after running rubocop.

--- lib/Algorithm/algorithm.rb
+++ lib/Algorithm/algorithm.rb
@@ -1,20 +1,24 @@
 # Defining a class Algorithm.
 class Algorithm
-
-  #defining a method to find the largest integer
-  def find_largest_integer(*integers)
-
+  # defining a method to find the largest integer
+  def find_largest_integer(integers)
+    largest = 0
     puts "The length of integers is #{integers.length}"
 
     integers.each do |integer_iteration|
 
-      puts "The integer = #{integers}"
-      puts "The integer_iteration = #{integer_iteration}"
+      if integer_iteration.is_a? Integer
+        if integer_iteration > largest
+          largest = integer_iteration
+        end
 
-      #largest = integer_iteration
-      puts integers.length
-      puts largest
+        puts "Integer Iteration #{integer_iteration}"
+        puts "largest #{largest}"
 
+      else
+        puts "Integer iteration #{integer_iteration} is not an integer"
+
+      end
 
     end
 
@@ -24,7 +28,7 @@ end
 
 # Calling this as a simple test
 
-array = [1, 4, 5, 6]
+array = [1.1, 4, 5, 6, 4, 6, 7, 9, 4, 4.1]
 
 puts "This is the length of the array #{array.length}"
 
