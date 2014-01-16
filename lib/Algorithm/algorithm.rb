52aa1f791f9fe88c8fd1734e4c78face8f7b65b0
Anand <anand@loremore.com>
2014-01-15 18:26:51 -0800

    refactored code to reduce the number of lines in a method.

--- lib/Algorithm/algorithm.rb
+++ lib/Algorithm/algorithm.rb
@@ -2,27 +2,38 @@
 class Algorithm
   # defining a method to find the largest integer
   def find_largest_integer(integers)
-    largest = 0
-    puts "The length of integers is #{integers.length}"
+    largest = initialize_and_length__of_array(integers)
 
     integers.each do |integer_iteration|
 
       if integer_iteration.is_a? Integer
-        if integer_iteration > largest
-          largest = integer_iteration
-        end
+        largest = integer_iteration if integer_iteration > largest
 
-        puts "Integer Iteration #{integer_iteration}"
-        puts "largest #{largest}"
+        print_iteration_largest(integer_iteration, largest)
 
       else
-        puts "Integer iteration #{integer_iteration} is not an integer"
+        input_not_integer(integer_iteration)
 
       end
 
     end
 
     largest
+  end
+
+  def initialize_and_length__of_array(integers)
+    largest = 0
+    puts "The length of integers is #{integers.length}"
+    largest
+  end
+
+  def input_not_integer(integer_iteration)
+    puts "Integer iteration #{integer_iteration} is not an integer"
+  end
+
+  def print_iteration_largest(integer_iteration, largest)
+    puts "Integer Iteration #{integer_iteration}"
+    puts "largest #{largest}"
   end
 end
 
