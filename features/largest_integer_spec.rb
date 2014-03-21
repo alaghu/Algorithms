902cbeb810f23674c480785daf9f49981fce220d
Anand <anand@loremore.com>
2014-03-21 06:47:14 -0700

    100% code coverage for largest integer. Modified the test cases to include chanrecters and null.

--- features/largest_integer_spec.rb
+++ features/largest_integer_spec.rb
@@ -2,33 +2,24 @@ require_relative 'spec_helper'
 
 def before_each(array)
   largest = LargestInteger.new
-  output = largest.find_largest_integer array
+  largest.find_largest_integer array
 end
 
 describe 'Largest Integer' do
 
-
-  it 'should return zero when input is zero' do
-    array = [0]
-
-    output = before_each(array)
-    output.should be 0
-
-  end
-
-  it 'should return zero when input is negative numbers' do
-    array = [-1, -2]
+  it 'should return zero when input is zero or negative' do
+    array = [0, -1, -2]
 
     output = before_each(array)
     output.should be 0
 
   end
 
-  it 'should find return two when input is one and two' do
-    array = [1, 2]
+  it 'should find return zero when input is nil or other characters' do
+    array = [nil, 'a', 'z', '!']
 
     output = before_each(array)
-    output.should be 2
+    output.should be 0
 
   end
 
@@ -36,7 +27,7 @@ describe 'Largest Integer' do
     array = [1.1, 4, 5, 6, 4, 6, 7, 19, 4, 19, 4.1]
 
     output = before_each(array)
-    output.should == 19
+    output.should be 19
 
   end
 
