027193a505573e0c0e9f9ab99bfee9a042d5fea3
Anand <anand@loremore.com>
2014-03-23 19:10:00 -0700

    failing if we have two test cases.

--- features/1_1_26_sorting_three_numbers_spec.rb
+++ features/1_1_26_sorting_three_numbers_spec.rb
@@ -2,19 +2,26 @@ require_relative 'spec_helper'
 
 describe 'Sorting three numbers' do
 
-  it 'should return 1 2 3 when 3 2 1 are the input' do
-    input_array = [3, 2, 1]
+  it 'should return same when the input is all same b=numbers' do
 
     sorted = SortThree.new
 
-    output = sorted.sort_the_numbers(1, 2, 3)
+    output = sorted.sort_the_numbers(1, 1, 1)
 
+    expect(output).to eq([1, 1, 1])
+
+  end
+
+  it 'should return 1 2 3 when 3 2 1 are the input' do
+
+    sorted = SortThree.new
+
+    output = sorted.sort_the_numbers(3, 2, 1)
 
     expect(output).to eq([1, 2, 3])
 
   end
 end
-
 
 # Deprecation of should method for array
 # output.should be [1, 2, 3]
