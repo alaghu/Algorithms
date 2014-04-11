4994964d0d309df01a637cc0d020e13b2e9f7cb4
Anand <anand@loremore.com>
2014-04-10 18:45:04 -0700

    added coveralls gem to the spec helper.

--- features/spec_helper.rb
+++ features/spec_helper.rb
@@ -1,14 +1,14 @@
 require 'simplecov'
-# require 'coveralls'
-#
-# Coveralls.wear!
-# #SimpleCov.start
-#
-# SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-#     SimpleCov::Formatter::HTMLFormatter,
-#     Coveralls::SimpleCov::Formatter
-# ]
+require 'coveralls'
+
+Coveralls.wear!
 SimpleCov.start
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+]
+
 
 require_relative '../lib/Algorithm/largest_integer'
 require_relative '../lib/Algorithm/1_1_26_sorting_three_numbers'
