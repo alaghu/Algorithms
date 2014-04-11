172cd157740926cbc66357498a570678baf2b5c2
Anand <anand@loremore.com>
2014-04-10 18:42:59 -0700

    rspec works only from root with the following rspec feature/

--- spec/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-require 'simplecov'
-require 'coveralls'
-
-Coveralls.wear!
-SimpleCov.start
-
-
-require_relative '../lib/Algorithm/largest_integer'
-require_relative '../lib/Algorithm/1_1_26_sorting_three_numbers'
