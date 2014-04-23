0c2f851ff5b2fcba019378e478c11668e6308511
Anand <anand@loremore.com>
2014-04-23 05:51:33 -0700

    some comments.

--- features/1_1_28_binary_search_spec.rb
+++ features/1_1_28_binary_search_spec.rb
@@ -12,13 +12,47 @@ describe 'Find a number using Binary Search' do
 
   end
 
-  # it 'should return 4 3 2 1 when 1 2 3 4 are the input' do
-  #
-  #   search_result = BinarySearch.new
-  #
-  #   position = search_result.find_the_number(1, [1, 2, 3, 4])
-  #
-  #   expect(position).to eq(1)
-  #
-  # end
+  it 'should return 2 when 3 found in 1 2 3 4' do
+
+    search_result = BinarySearch.new
+
+    position = search_result.find_the_number(3, [1, 2, 3, 4, 5])
+
+    expect(position).to eq(2)
+
+  end
+
+  it 'should return 1 when 2 found in 1, 2, 3, 4 ' do
+
+    search_result = BinarySearch.new
+
+    position = search_result.find_the_number(2, [1, 2, 3, 4])
+
+    expect(position).to eq(1)
+
+  end
+
+
+  it 'should return 3 when 4 found in 1, 2, 3, 4 ' do
+
+    search_result = BinarySearch.new
+
+    position = search_result.find_the_number(4, [1, 2, 3, 4])
+
+    expect(position).to eq(3)
+
+  end
+
+  it 'should return 3 when 4 found in 1, 2, 3, 4 ..10  ' do
+
+    search_result = BinarySearch.new
+
+    position = search_result.find_the_number(4, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+    expect(position).to eq(3)
+
+  end
+
+
+
 end
--- lib/Algorithm/1_1_28_binary_search.rb
+++ lib/Algorithm/1_1_28_binary_search.rb
@@ -1,7 +1,31 @@
 # To search using binary search
 class BinarySearch
   def find_the_number(number_to_find, array_sorted)
-    -1
-  end
 
+    lo = 0
+    hi = array_sorted.length - 1
+    mid = (hi - lo) / 2
+
+    # First half of the search array
+    (lo..mid).each do |element|
+      if number_to_find == array_sorted[element]
+        puts "found #{number_to_find} at #{element}"
+        return element
+      end
+    end
+
+
+    # Second half of the search array
+    (mid+1..hi).each do |element|
+      if number_to_find == array_sorted[element]
+        puts "Found #{array_sorted[element]} at #{element}"
+        return element
+      end
+    end
+
+
+    return -1
+
+
+  end
 end
