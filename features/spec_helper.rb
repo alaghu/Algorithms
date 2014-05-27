f627b0e0bec15533f058bb99c5c41815755e1bf3
Anand <anand@loremore.com>
2014-05-26 19:41:53 -0700

    had to remove the _sorting_four_numbers from spec helper which lists all the spec files.

--- features/spec_helper.rb
+++ features/spec_helper.rb
@@ -11,6 +11,5 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
 
 require_relative '../lib/Algorithm/largest_integer'
 require_relative '../lib/Algorithm/1_1_26_sorting_three_numbers'
-require_relative '../lib/Algorithm/1_1_26_sorting_four_numbers'
 require_relative '../lib/Algorithm/1_1_26_sorting_n_numbers'
 require_relative '../lib/Algorithm/1_1_28_binary_search'
